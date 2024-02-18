6890e69f7126c5e29e034fd0ddb3be2d570caf78
Pranjal Bareth <pranjalnirmala@gmail.com>
2024-02-18 20:51:20 +0530

    Add a file

---
+++ welcome.py
@@ -0,0 +1,8 @@
+print("Welcome to DevOps!")
+print("Basic DevOps tools include:")
+print("- Jenkins")
+print("- Docker")
+print("- Kubernetes")
+print("- Git")
+print("- Ansible")
+print("- Terraform")
